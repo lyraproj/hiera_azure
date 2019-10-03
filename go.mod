c1d1c08a6731f1b895548937e352d1f271eda031
Thomas Hallgren <thomas@tada.se>
2019-10-03 10:06:28 +0200

    Change use of Go plugin to Hiera RESTful plugin
    This commit transforms the vaultlookupkey plugin into a RESTful service
    that can be used as a Hiera plugin.

--- go.mod
+++ go.mod
@@ -8,13 +8,7 @@ require (
 	github.com/Azure/go-autorest/autorest/azure/auth v0.3.0 // indirect
 	github.com/Azure/go-autorest/autorest/to v0.3.0 // indirect
 	github.com/Azure/go-autorest/autorest/validation v0.2.0 // indirect
-	github.com/lyraproj/dgo v0.0.0-20190918142629-ee8b686ca69b
-	github.com/lyraproj/hiera v0.0.0-20190820132249-b08bb003a3b6
-	github.com/lyraproj/issue v0.0.0-20190606092846-e082d6813d15
-	github.com/lyraproj/pcore v0.0.0-20190918201925-7e14d50f3d7d
+	github.com/lyraproj/hierasdk v0.0.0-20191002210033-6ab2cc3bcf0e
 )
 
-replace (
-	github.com/Azure/go-autorest => github.com/Azure/go-autorest v12.3.0+incompatible
-	github.com/lyraproj/hiera => github.com/thallgren/hiera v0.0.0-20190920152706-3eecfa8da3f1
-)
+replace github.com/Azure/go-autorest => github.com/Azure/go-autorest v12.3.0+incompatible
--- vaultlookupkey.go
+++ vaultlookupkey.go
@@ -2,51 +2,53 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/Azure/azure-sdk-for-go/profiles/latest/keyvault/keyvault"
-	kvauth "github.com/Azure/azure-sdk-for-go/services/keyvault/auth"
+	"github.com/Azure/azure-sdk-for-go/services/keyvault/auth"
 	"github.com/Azure/go-autorest/autorest"
-	"github.com/lyraproj/hiera/hieraapi"
-	"github.com/lyraproj/issue/issue"
-	"github.com/lyraproj/pcore/px"
-	"github.com/lyraproj/pcore/types"
+	"github.com/lyraproj/hierasdk/hiera"
+	"github.com/lyraproj/hierasdk/plugin"
+	"github.com/lyraproj/hierasdk/register"
+	"github.com/lyraproj/hierasdk/vf"
 )
 
-func init() {
-	hieraapi.RegisterLookupKey(`azure_key_vault`, AzureKeyVaultLookupKey)
+func main() {
+	register.LookupKey(`azure_key_vault`, AzureKeyVaultLookupKey)
+	plugin.ServeAndExit()
 }
 
 // AzureKeyVaultLookupKey looks up a single value from an Azure Key Vault
-func AzureKeyVaultLookupKey(hc hieraapi.ProviderContext, key string, options map[string]px.Value) px.Value {
+func AzureKeyVaultLookupKey(hc hiera.ProviderContext, key string) vf.Data {
 	if key == `lookup_options` {
-		hc.NotFound()
+		return nil
 	}
-	vaultName, ok := options[`vault_name`]
+	vaultName, ok := hc.StringOption(`vault_name`)
 	if !ok {
-		panic(px.Error(hieraapi.MissingRequiredOption, issue.H{`option`: `vault_name`}))
+		panic(fmt.Errorf(`missing required provider option 'vault_name'`))
 	}
 	var authorizer autorest.Authorizer
 	var err error
 	if os.Getenv("AZURE_TENANT_ID") != "" && os.Getenv("AZURE_CLIENT_ID") != "" && os.Getenv("AZURE_CLIENT_SECRET") != "" {
-		authorizer, err = kvauth.NewAuthorizerFromEnvironment()
+		authorizer, err = auth.NewAuthorizerFromEnvironment()
 	} else {
-		authorizer, err = kvauth.NewAuthorizerFromCLI()
+		authorizer, err = auth.NewAuthorizerFromCLI()
 	}
 	if err != nil {
 		panic(err)
 	}
 	client := keyvault.New()
 	client.Authorizer = authorizer
-	resp, err := client.GetSecret(context.Background(), "https://"+vaultName.String()+".vault.azure.net", key, "")
+	resp, err := client.GetSecret(context.Background(), "https://"+vaultName+".vault.azure.net", key, "")
 	if err != nil {
 		if ResponseWasStatusCode(resp.Response, http.StatusNotFound) {
-			hc.NotFound()
+			return nil
 		}
 		panic(err)
 	}
-	return types.WrapString(*resp.Value)
+	return vf.ToData(*resp.Value)
 }
 
 func ResponseWasStatusCode(resp autorest.Response, statusCode int) bool {
